bbe481b7d87e77579e733cba8664548fa0526ec3
Yi Hong <jessicalee_88@hotmail.com>
2012-06-18 13:15:51 +0800

    Added barcode scanner in transact.js

--- Resources/transact.js
+++ Resources/transact.js
@@ -1,2 +1,34 @@
 // initiate the window for 'transact' tab
-var win = Titanium.UI.currentwindow;
+var win = Titanium.UI.currentWindow;
+
+win.backgroundColor = 'white'
+
+var label = Ti.UI.createLabel();
+win.add(label);
+win.open();
+
+var titaniumBarcode = require('com.mwaysolutions.barcode');
+
+titaniumBarcode.scan({
+  success:function(data) {
+    if(data && data.barcode) {
+      var label = Titanium.UI.createLabel({
+        text:'Barcode: ' + data.barcode,
+        textAlign:'center',
+        width:'auto'
+      });
+
+      win.add(label);
+    } else {
+      alert(JSON.stringify(data));
+    }
+  },
+
+  error:function(err) { 
+    alert("Error!! " + err); 
+  },
+
+  cancel:function() { 
+    alert("cancel"); 
+  }
+});
