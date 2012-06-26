28c6c61167188d52e3d13fdb38276aff492ec20e
Yi Hong <jessicalee_88@hotmail.com>
2012-06-26 17:53:27 +0800

    Added a display page in the transac.js Added images for transac

--- Resources/transact.js
+++ Resources/transact.js
@@ -1,13 +1,49 @@
 // initiate the window for 'transact' tab
-var win = Titanium.UI.currentWindow;
+var transactWin = Titanium.UI.currentWindow;
 
 var screenHeight = Ti.Platform.displayCaps.platformHeight;
 var screenWidth = Ti.Platform.displayCaps.platformWidth;
-
 function GetWidth(value) {
     return parseInt((screenWidth * value) / 320);
 }
 
+var sellABookImage = Ti.UI.createImageView({
+	image: 'images/Salesbook.png',
+	width: '300dp',
+	height:	'130dp',
+	left: '10dp',
+	right: '10dp',
+	top: '10dp'
+});
+transactWin.add(sellABookImage);
+sellABookImage.addEventListener('click', function(e){
+	bacodeInputTypeWin.open();
+});
+
+var sellingListImage = Ti.UI.createImageView({
+	image: 'images/sellingbooks1.png',
+	width: '300dp',
+	height:	'130dp',
+	left: '10dp',
+	right: '10dp',
+	top: '150dp'
+});
+transactWin.add(sellingListImage);
+
+var buyingListImage = Ti.UI.createImageView({
+	image: 'images/blank.png',
+	width: '300dp',
+	height:	'130dp',
+	left: '10dp',
+	right: '10dp',
+	top: '290dp'
+});
+transactWin.add(buyingListImage);
+
+//Choosing Barcode
+var bacodeInputTypeWin = Ti.UI.createWindow({
+	backgroundColor: '#FFFFFF',
+});
 var typeBarcodeTextField = Ti.UI.createTextField({
 	hintText: '13 ISBN w\/o \'-\'',
 	font:{
@@ -19,7 +55,7 @@ var typeBarcodeTextField = Ti.UI.createTextField({
 	height: '40dp',
 	keyboardType: Ti.UI.KEYBOARD_NUMBER_PAD
 });
-win.add(typeBarcodeTextField);
+bacodeInputTypeWin.add(typeBarcodeTextField);
 
 var submitISBNButton = Ti.UI.createButton({
 	title: 'Submit',
@@ -41,7 +77,7 @@ submitISBNButton.addEventListener('click', function(e){
 		alert('Invalid isbn number. Check that the isbn number is 13 digit long. And remove all \'-\'')
 	}
 });
-win.add(submitISBNButton);
+bacodeInputTypeWin.add(submitISBNButton);
 
 var sellingDetailsWin = Ti.UI.createWindow({
 	backgroundColor : '#FFFFFF',
@@ -69,6 +105,6 @@ var bacodeScanningWin = Titanium.UI.createWindow({
 	url : 'barcodeScanner.js',
 });
 
-win.add(scannerWinButton);
+bacodeInputTypeWin.add(scannerWinButton);
 
 
