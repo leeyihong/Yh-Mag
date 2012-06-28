63328ae37fef2449f34ae6d4c2bd339268e33642
Yi Hong <jessicalee_88@hotmail.com>
2012-06-28 16:40:06 +0800

    Allow camera function save photo

--- Resources/transact.js
+++ Resources/transact.js
@@ -17,7 +17,7 @@ var sellABookImage = Ti.UI.createButton({
 	top: '10dp'
 });
 sellABookImage.addEventListener('click', function(e){
-	bacodeInputTypeWin.open();
+	openCameraDialog.show();
 });
 
 var sellingListImage = Ti.UI.createButton({
@@ -72,23 +72,94 @@ trasacOptionTable.appendRow(buyingListRow);
 
 transactWin.add(trasacOptionTable);
 
+
+var openCameraDialog = Titanium.UI.createOptionDialog({
+	title: 'Choose an image source :',
+	options: ['Camera','Photo Gallery', 'Cancel'],
+	cancel:2
+});
+
+var imgView = Titanium.UI.createImageView({
+	top: '5dp',
+	left: GetWidth(60),
+	width: '200dp',
+	height: '200dp'
+});
+
+openCameraDialog.addEventListener('click',function(e) {
+	Ti.API.info('You selected ' + e.index);
+	if(e.index == 0){
+		
+		//from the camera
+		Titanium.Media.showCamera({
+			
+			success:function(event){
+			var image = event.media;
+			if(event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO) {
+				//set image view
+				
+				imgView.image = image;
+				bacodeInputTypeWin.open();
+				}
+			},
+			
+			cancel:function(){
+				//getting image from camera was cancelled
+			},
+			
+			error:function(error){
+				//create alert
+				var a = Titanium.UI.createAlertDialog({
+					title:'Camera'
+				});
+				// set message
+				if (error.code == Titanium.Media.NO_CAMERA) {
+					a.setMessage('Device does not have image recording capabilities');
+				} else {
+					a.setMessage('Unexpected error: ' + error.code);
+				}
+			// show alert
+			a.show();
+			},
+			
+		allowImageEditing:true,
+		saveToPhotoGallery:false
+		});
+	} else {
+		//cancel was tapped
+		//user opted not to choose a photo
+	}
+});
+
 //Choosing Barcode
 var bacodeInputTypeWin = Ti.UI.createWindow({
 	backgroundColor: '#FFFFFF',
 	modal: true
 });
+
+var bookTitleImage = Ti.UI.createImageView({
+	url: 'images/Book Image.png',
+	width: '150dp',
+	height: '60dp'
+})
+
+var nextStepImage = Ti.UI.createImageView({
+	url: 'images/Next Step.png',
+	top: '20dp',
+	width: '150dp',
+	height: '60dp'
+})
+
 var typeBarcodeTextField = Ti.UI.createTextField({
 	hintText: '13 ISBN w\/o \'-\'',
 	font:{
 		fontSize: '14dp',
 	},
 	left: GetWidth(70),
-	top: '50dp',
 	width: GetWidth(130),
 	height: '40dp',
 	keyboardType: Ti.UI.KEYBOARD_NUMBER_PAD
 });
-bacodeInputTypeWin.add(typeBarcodeTextField);
 
 var submitISBNButton = Ti.UI.createButton({
 	title: 'Submit',
@@ -96,11 +167,9 @@ var submitISBNButton = Ti.UI.createButton({
 		fontSize: '14dp',
 	},
 	left: GetWidth(200),
-	top:'50dp',
 	width: '69dp',
 	height: '40dp'
 });
-
 var isbnNo;
 submitISBNButton.addEventListener('click', function(e){
 	if(typeBarcodeTextField.value.length == 13){
@@ -110,7 +179,6 @@ submitISBNButton.addEventListener('click', function(e){
 		alert('Invalid isbn number. Check that the isbn number is 13 digit long. And remove all \'-\'')
 	}
 });
-bacodeInputTypeWin.add(submitISBNButton);
 
 var sellingDetailsWin = Ti.UI.createWindow({
 	backgroundColor : '#FFFFFF',
@@ -125,7 +193,6 @@ var scannerWinButton = Ti.UI.createButton({
 		fontSize: '14dp',
 	},
 	left: GetWidth(68),
-	top:'100dp',
 	width: GetWidth(200),
 	height: '40dp'	
 });
@@ -138,6 +205,53 @@ var bacodeScanningWin = Titanium.UI.createWindow({
 	url : 'barcodeScanner.js',
 });
 
-bacodeInputTypeWin.add(scannerWinButton);
+// Scrollable table
+var barcodeOptionTable = Ti.UI.createTableView({
+	height: 'auto',
+	separatorColor: 'transparent',
+});
+
+//Image Title Row
+var imageTitleRow = Ti.UI.createTableViewRow({
+	height: 'auto',
+});
+imageTitleRow.center;
+imageTitleRow.add(bookTitleImage);
+barcodeOptionTable.appendRow(imageTitleRow);
+
+//Image Row
+var imageRow = Ti.UI.createTableViewRow({
+	height: 'auto',
+});
+imageRow.center;
+imageRow.add(imgView);
+barcodeOptionTable.appendRow(imageRow);
+
+//Next Step Row
+var nextStepRow = Ti.UI.createTableViewRow({
+	height: 'auto',
+});
+nextStepRow.center;
+nextStepRow.add(nextStepImage);
+barcodeOptionTable.appendRow(nextStepRow);
+
+//Input Barcode Row
+var inputBarcodeRow = Ti.UI.createTableViewRow({
+	height: 'auto',
+});
+inputBarcodeRow.add(typeBarcodeTextField);
+inputBarcodeRow.add(submitISBNButton);
+barcodeOptionTable.appendRow(inputBarcodeRow);
+
+//Barcode Scanner Row
+var barcodeScannerRow = Ti.UI.createTableViewRow({
+	height: 'auto',
+});
+barcodeScannerRow.center;
+barcodeScannerRow.add(scannerWinButton);
+barcodeOptionTable.appendRow(barcodeScannerRow);
+
+bacodeInputTypeWin.add(barcodeOptionTable);
+
 
 
