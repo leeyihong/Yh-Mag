9b9a9648c5f8c44a461b971500056327f9882f96
Yi Hong <jessicalee_88@hotmail.com>
2012-07-03 11:44:28 +0800

    adding table into search table

--- Resources/search.js
+++ Resources/search.js
@@ -1,2 +1,42 @@
 // initiate the window for 'search' tab
-var win = Titanium.UI.currentwindow;
+var searchWin = Ti.UI.currentWindow;
+var currentTab = Ti.UI.currentTab;
+
+var facultiesList = [
+"Arts & Social Sciences",
+"Business",
+"Computing",
+"Dentistry",
+"Design & Environment",
+"Engineering",
+"Law",
+"Medicine",
+"Science"]; 
+for (var i=0; i<facultiesList.length; i++){
+	var row = Ti.UI.createTableViewRow({});
+	
+	var textlabel = Ti.UI.createLabel({
+		text: facultiesList[i],
+		color: '#000014',
+		font:{
+			fontSize: '14dp', 
+			fontFamily: 'Helvetica',
+			fontWeight:'bold'
+		},
+		textAlign:'left',
+		left:'10dp',
+		height: '50dp'
+	});
+	row.add(textlabel);
+	facultiesList[i] = row;
+};
+var facultyTable = Titanium.UI.createTableView({
+	data:facultiesList,
+	font:{
+		fontSize: '14dp', 
+		fontFamily: 'Helvetica',
+		fontWeight:'bold'
+	},
+	color: '#000014',
+}); 
+searchWin.add(facultyTable);
