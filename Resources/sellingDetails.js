896ced7ded90d087076af2eaf070099bc1bf99f2
Yi Hong <jessicalee_88@hotmail.com>
2012-07-05 15:50:27 +0800

    committing....

--- Resources/sellingDetails.js
+++ Resources/sellingDetails.js
@@ -538,6 +538,16 @@ var publishDialog = Ti.UI.createAlertDialog({
 	buttonNames : ['Confirm', 'Cancel'],
 });
 publishButton.addEventListener('click', function() {
+		
+	if(!moduleCodeField.value){
+		alert('Please enter your module code');
+		return;	
+	}
+	if((!priceField.value)||(priceField.value.length <= 1)){
+		alert('Please enter the price');
+		return;
+	}
+	
 	publishDialog.show();
 });
 
@@ -549,15 +559,7 @@ var activityIndicator = Ti.UI.createActivityIndicator({
 });
 
 publishDialog.addEventListener('click', function(e) {
-	
-	if(!moduleCodeField.value){
-		alert('Please enter your module code');
-		return;	
-	}
-	if((!priceField.value)||(!priceField.value.length <= 1)){
-		alert('Please enter the price');
-		return;
-	}
+
 	
 	if (e.index == 0) {// clicked "Confirm"
 		activityIndicator.show();
